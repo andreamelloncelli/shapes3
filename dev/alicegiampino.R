8832aaaa941435f83e0ea37b5cce33c26e601262
Alice Giampino <giampinoalice@gmail.com>
2020-06-25 12:06:36 +0200

    Alice

--- dev/alicegiampino.R
+++ dev/alicegiampino.R
@@ -89,6 +89,111 @@ counter_to_pi(counter, n)
 
 map(points, in_circle) %>%
   reduce(sum) %>%
-  counter_to_pi((n))
+  counter_to_pi(n)
+
+# mclapply ----------------------------------------------------------------
+
+library(tidyverse)
+library(tictoc)
+library(parallel)
+
+# tic inizia conteggio tempo, toc lo ferma
+
+tic()
+lapply(points, in_circle) %>%
+  unlist() %>%
+  sum() %>%
+  counter_to_pi(n)
+toc()
+# reduce non è ottimizzato perché all'interno ha ciclo for
+
+# funzione della libreria parallel, usa più core per farlo
+tic()
+mclapply(points, in_circle, mc.cores=3, mc.preschedule = T) %>%
+  unlist() %>%
+  sum() %>%
+  counter_to_pi(n)
+toc()
 
 
+# preschedule = T, array viene diviso nel numero di core
+
+
+# foreach -----------------------------------------------------------------
+
+library(foreach)
+library(iterators)
+library(doParallel)
+
+tic()
+mcOptions <- list(preschedule=T, set.seed=F)
+
+foreach( p = iter(points),
+         .combine = sum,
+         .multicombine = T,
+         .inorder = F,
+         .init = 0,
+         .options.multicore = mcOptions,
+         .verbose = F) %do% {
+  in_circle(p)
+}
+toc()
+
+# .combine che operazione usare per il reduce
+# .multicombine fa tutto alla volta divisa
+# .inorder se deve fare in ordine o no
+
+# foreach lavora in modo sequenziale col do, e in parallelo con dopar
+
+registerDoParallel(cores=3)
+getDoParWorkers() # crea un ambiente dove lavorare in parallelo
+
+tic()
+mcOptions <- list(preschedule=T, set.seed=F)
+
+foreach( p = iter(points),
+         .combine = sum,
+         .multicombine = T,
+         .inorder = F,
+         .init = 0,
+         .options.multicore = mcOptions,
+         .verbose = F) %dopar% {
+           in_circle(p)
+         }
+toc()
+
+
+# future ------------------------------------------------------------------
+
+library(future)
+
+plan(multisession)
+
+tic()
+map(points, in_circle) %>%
+  reduce(sum) %>%
+  counter_to_pi(n)
+toc()
+
+# permetto di fare a ma nel mentre posso eseguire altre operazioni
+# definendo a come feature, non aspetto che a sia pronto
+# il calcolo va in background
+
+a %<-% {
+
+  map(points, in_circle) %>%
+  reduce(sum) %>%
+  counter_to_pi(n)
+
+}
+
+# forma esplicita:
+a <- future({
+
+  lapply(p, in_circle) %>%
+    reduce(sum) %>%
+    counter_to_pi(n)
+
+}, globals = list( p = points))
+
+# con globals posso ridefinire degli oggetti
