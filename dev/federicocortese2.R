6338c3a0a7b78f3442e2ada1d6761c9b1bd6fc75
unknown <cortese.federico@live.it>
2020-06-25 11:20:47 +0200

    ciao ciao

--- dev/federicocortese2.R
+++ dev/federicocortese2.R
@@ -24,13 +24,22 @@ counter_to_pi=function(count,total){
 
 count=0
 
-L=length(points)
-for(i in 1:L){
-  if(in_circle(point[[i]])){
-    count=count+1
-  }
+# L=length(points)
+# for(i in 1:L){
+#   if(in_circle(point[[i]])){
+#     count=count+1
+#   }
+# }
+
+counter=0
+for(point_indx in seq_along(points)){
+  point=points[[points_indx]]
+  counter=counter+in_circle(point)
+
 }
 
+
+counter_to_pi(counter,n)
 
 
 
